d735480a25800354a0747f813c40ed01c83cd5b9
patrickeharris <patrickeharris404@gmail.com>
2022-09-05 11:58:27 -0500

    Mobile ui fixes

--- road-trip-frontend/src/components/feature/Feature.jsx
+++ road-trip-frontend/src/components/feature/Feature.jsx
@@ -1,19 +1,35 @@
 import React from 'react'
 import styles from './feature.module.css'
 
-const Feature = ({title, text}) => {
-
-    return (
-        <div className={styles.feature}>
-            <div className={styles.featureTitle}>
-                <div />
-                <h1>{title}</h1>
+const Feature = ({title, text, subfeature}) => {
+    if(subfeature) {
+        return (
+            <div className={styles.subfeature}>
+                <div className={styles.featureTitle}>
+                    <div/>
+                    <h1>{title}</h1>
+                </div>
+                <div className={styles.featureText}>
+                    <div className={styles.subFeatureText}>
+                        <p>{text}</p>
+                    </div>
+                </div>
             </div>
-            <div className={styles.featureText}>
-                <p>{text}</p>
+        )
+    }
+    else {
+        return (
+            <div className={styles.feature}>
+                <div className={styles.featureTitle}>
+                    <div/>
+                    <h1>{title}</h1>
+                </div>
+                <div className={styles.featureText}>
+                    <p>{text}</p>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Feature
