b2069be2bf44b5cb1066c3e71898b346b9087e1a
patrickeharris <patrickeharris404@gmail.com>
2022-09-27 18:30:38 -0500

    Toast notification stack

--- road-trip-frontend/src/containers/register/Register.jsx
+++ road-trip-frontend/src/containers/register/Register.jsx
@@ -3,6 +3,8 @@ import styles from './register.module.css'
 import globalStyles from "../container.module.css";
 import {myAxios} from "../../util/helper";
 import bcrypt from 'bcryptjs'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const signUp=(user)=>{
     console.log("User");
@@ -22,6 +24,7 @@ const Register = () => {
     const [lastName, setLastName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
     const handleSubmit = async () => {
         const salt = bcrypt.genSaltSync(10)
@@ -40,6 +43,16 @@ const Register = () => {
             setLastName("");
             setEmail("");
             setPassword("");
+            setConfirmPassword("");
+            toast.success('Successfully Registered!', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
         } catch (err) {
             if (!err?.response) {
                 console.log("No Server Response");
@@ -52,6 +65,7 @@ const Register = () => {
 
     return (
         <div className={globalStyles.sectionPadding}>
+            <ToastContainer />
             <div className={styles.register}>
                 <div className={styles.registerContent}>
                     <h1 className={globalStyles.gradientText}>Sign Up</h1>
@@ -60,7 +74,7 @@ const Register = () => {
                         <input type="text" placeholder="Last name" onChange={(e) => setLastName(e.target.value)} value={lastName}/>
                         <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} value={email}/>
                         <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password}/>
-                        <input type="password" placeholder="Confirm Password"/>
+                        <input type="password" placeholder="Confirm Password" onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword}/>
                         <button type="button" onClick={handleSubmit}>Sign Up</button>
                     </div>
                 </div>
