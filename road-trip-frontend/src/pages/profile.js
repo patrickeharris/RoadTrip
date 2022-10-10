8b538cbf7a9200b9523b45e59c358633c86afdb1
Mia Gortney <mia_gortney1@baylor.edu>
2022-10-09 20:35:00 -0500

    Pipeline fix

--- road-trip-frontend/src/pages/profile.js
+++ road-trip-frontend/src/pages/profile.js
@@ -3,7 +3,6 @@ import Link from '@material-ui/core/Link';
 import styles from "./styles/index.module.css";
 import {Navbar} from "../components";
 import {Footer} from "../containers";
-import Profile from "../containers"
 
 require('dotenv').config();
 
@@ -13,7 +12,6 @@ function ProfilePage() {
         <div className={styles.wrapper}>
             <div className={styles.gradient__bg}>
                 <Navbar />
-                <Profile />
                 <Footer />
             </div>
         </div>
