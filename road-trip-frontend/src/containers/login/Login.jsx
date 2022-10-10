1fb4246dcdf7662ad3b4243c661c556e00e419f6
Mia Gortney <mia_gortney1@baylor.edu>
2022-10-09 20:24:52 -0500

    Implemented Login

--- road-trip-frontend/src/containers/login/Login.jsx
+++ road-trip-frontend/src/containers/login/Login.jsx
@@ -8,7 +8,7 @@ import React, {useState} from 'react'
 import styles from './login.module.css'
 import globalStyles from "../container.module.css";
 import {myAxios} from "../../util/helper";
-import {toast} from "react-toastify";
+import {toast, ToastContainer} from "react-toastify";
 
 const Login = () => {
 
@@ -17,29 +17,27 @@ const Login = () => {
 
     const handleSubmit = async () => {
 
-        if (global.loggedIn === true) {
+        if (window.localStorage.getItem('loggedIn') === 'true') {
             if (confirm("You are already logged in. Do you want to log out?")) {
-                global.loggedIn = false;
+                window.localStorage.setItem('loggedIn', 'false');
             }
         } else {
 
             try {
-                const response = await myAxios.get("/register/users");
-                const info = JSON.parse(response);
+                const response = (await myAxios.get("/register/users")).data;
                 let found = false;
                 let index;
 
-                for (let i = 0; i < info.email && !found; i++) {
-                    if (info[i] === email) {
-                        index = i;
+                for (let i = 0; i < response.length; i++) {
+                    if (response[i].email === email) {
                         found = true;
+                        index = i;
                     }
                 }
 
-                if (found) {
-                    if (info.password[index] === password) {
-
-                        global.loggedIn = true;
+                if (found === true) {
+                    if (response[index].password === password) {
+                        window.localStorage.setItem('loggedIn', 'true');
                         toast.success('Successfully Logged In!', {
                             position: "top-right",
                             autoClose: 5000,
@@ -49,12 +47,14 @@ const Login = () => {
                             draggable: true,
                             progress: undefined,
                         });
+                        window.localStorage.setItem('curUser', response[index].user_id);
+                        window.location.replace("/");
                     } else {
                         alert("This is the wrong password. Please try again!")
                     }
                 } else {
                     if (confirm("This email is not registered. Do you want to be directed to the sign up page?")) {
-
+                        window.location.replace("/register");
                     } else {
                         alert("Please try again!")
                     }
@@ -63,6 +63,7 @@ const Login = () => {
             } catch (err) {
                 if (!err?.response) {
                     console.log("No Server Response");
+                    console.log(err);
                 } else {
                     console.log("Login Failed");
                     console.log(err?.response);
@@ -74,13 +75,14 @@ const Login = () => {
 
     return (
         <div className={globalStyles.sectionPadding}>
+            <ToastContainer />
             <div className={styles.login}>
                 <div className={styles.loginContent}>
                     <h1 className={globalStyles.gradientText}>Sign In</h1>
                     <div className={styles.loginInput}>
                         <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} value={email}/>
                         <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password}/>
-                        <button type="button" onClick={handleSubmit}><a href="./">Sign In</a></button>
+                        <button type="button" onClick={handleSubmit}>Sign In</button>
                     </div>
                 </div>
             </div>
