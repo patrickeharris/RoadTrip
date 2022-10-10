f9342bb7d63fb798b406c56896b6ddca8c89164e
Jack Hale <jackhale@gmail.com>
2022-10-10 01:40:14 +0000

    Updated profile.js

--- road-trip-frontend/src/pages/profile.js
+++ road-trip-frontend/src/pages/profile.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import Link from '@material-ui/core/Link';
-import styles from "./styles/index.module.css";
+import styles from './styles/index.module.css';
 import {Navbar} from "../components";
-import {Footer} from "../containers";
+import {Footer, Profile} from "../containers";
 
 require('dotenv').config();
 
 function ProfilePage() {
     return (
-
         <div className={styles.wrapper}>
             <div className={styles.gradient__bg}>
                 <Navbar />
-                <div className={styles.white__txt}>
-                    This is the user profile page!
-                </div>
+                <Profile />
                 <Footer />
             </div>
         </div>
