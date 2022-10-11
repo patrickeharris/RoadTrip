9eb504c444656fdfbb9274c37cfc0cdf8ca3050c
Mia Gortney <mia_gortney1@baylor.edu>
2022-10-11 15:30:01 -0500

    Fixed Rating

--- road-trip-frontend/src/components/rating/Rating.jsx
+++ road-trip-frontend/src/components/rating/Rating.jsx
@@ -1,13 +1,14 @@
 import React, {useState} from "react";
-import axios from "axios";
 import styles from "./rating.module.css";
 import {Star} from "../index";
 import {myAxios} from "../../util/helper";
+import {toast, ToastContainer} from "react-toastify";
 
 const Rating = () => {
     const [rating, setRating] = useState(0);
     const [stopRating, setStopRating] = useState(0);
     const [message, setMessage] = useState('');
+    const [trip_id, setTrip_Id] = useState("");
     const [ratingStars, setRatingStars] = useState([{
         title: "1 star", isToggled: false
     }, {
@@ -52,7 +53,7 @@ const Rating = () => {
         setMessage(event.target.value);
     }
 
-    const handleFormSubmit = () => {
+    const handleFormSubmit = async() => {
         if (rating === 0) {
             alert("Please enter a star rating for your trip!")
             return
@@ -68,49 +69,73 @@ const Rating = () => {
             return
         }
 
-        axios.post("/rating/add", {
-            "trip_id": window.localStorage.getItem('curTrip'), "score": rating, "stopScore": stopRating, "message": message
-        })
-            .then((response) => {
-                console.log(response)
-                alert("Thank you for taking the time to give feedback!")
-                window.localStorage.setItem('curTrip', null);
-                window.location.href = "/"
-            })
+        setTrip_Id(window.localStorage.getItem('curTrip'));
+
+        const response = await myAxios.post(
+            "/rating/add",
+            JSON.stringify({trip_id, rating, stopRating, message}),
+            {
+                headers: {"Content-Type": "application/json",
+                    'Access-Control-Allow-Origin' : '*',
+                    'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE',},
+                withCredentials: true,
+            }
+        );
+
+        setRating("");
+        setStopRating("");
+        setMessage("");
+
+        toast.success('Successfully Rated Trip!', {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
+
+        window.location.replace("/trip-dashboard");
     }
 
-    return (<div className={styles.rateTrip__wrapper}>
+    return (
+        <div>
+            <ToastContainer />
+            <div className={styles.rateTrip__wrapper}>
 
-        <div className={styles.rateTrip__header}>
-            How would you rate your trip?
-        </div>
-        <div className={styles.rateTrip__formWrapper}>
+                <div className={styles.rateTrip__header}>
+                    How would you rate your trip?
+                </div>
+                <div className={styles.rateTrip__formWrapper}>
 
-            <div className={styles.rateTrip__subHeader}>
-                Overall Trip Rating
-            </div>
-            <div className={styles.rateTrip__starSelectWrapper}>
-                {ratingStars.map((star, index) => {
-                    return (<Star title={star.title} isToggled={star.isToggled}
-                                  onClick={() => handleStarClick(index)}/>)
-                })}
-            </div>
-            <div className={styles.rateTrip__subHeader}>
-                Overall Stop Rating
-            </div>
-            <div className={styles.rateTrip__starSelectWrapper}>
-                {stopRatingStars.map((star, index) => {
-                    return (<Star title={star.title} isToggled={star.isToggled}
-                                  onClick={() => handleStopStarClick(index)}/>)
-                })}
-            </div>
-            <div>
+                    <div className={styles.rateTrip__subHeader}>
+                        Overall Trip Rating
+                    </div>
+                    <div className={styles.rateTrip__starSelectWrapper}>
+                        {ratingStars.map((star, index) => {
+                            return (<Star title={star.title} isToggled={star.isToggled}
+                                          onClick={() => handleStarClick(index)}/>)
+                        })}
+                    </div>
+                    <div className={styles.rateTrip__subHeader}>
+                        Overall Stop Rating
+                    </div>
+                    <div className={styles.rateTrip__starSelectWrapper}>
+                        {stopRatingStars.map((star, index) => {
+                            return (<Star title={star.title} isToggled={star.isToggled}
+                                          onClick={() => handleStopStarClick(index)}/>)
+                        })}
+                    </div>
+                    <div>
                 <textarea placeholder="Tell us a little bit more about how we did"
                           className={styles.rateTrip__textArea} onChange={handleMessageChange}/>
+                    </div>
+                </div>
+                <div className={styles.rateTrip__formSubmitButton} onClick={handleFormSubmit}>Submit</div>
             </div>
         </div>
-        <div className={styles.rateTrip__formSubmitButton} onClick={handleFormSubmit}>Submit</div>
-    </div>)
+        )
 }
 
 export default Rating;
