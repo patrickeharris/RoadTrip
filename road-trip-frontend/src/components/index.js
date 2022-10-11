45b84d0352d931a026f8d44b3d22789b61c4431d
Mia Gortney <mia_gortney1@baylor.edu>
2022-10-11 13:57:23 -0500

    Quick fix

--- road-trip-frontend/src/components/index.js
+++ road-trip-frontend/src/components/index.js
@@ -3,4 +3,3 @@ export {default as Card} from './card/Card';
 export {default as TripList} from './tripList/TripList'
 export {default as Star} from './rating/Star'
 export {default as Rating} from './rating/Rating'
-export {default as Profile} from './profile/Profile'
