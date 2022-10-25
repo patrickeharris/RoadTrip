505d34774b90f321cc5a5561ad415bb7ef863867
Mia Gortney <mia_gortney1@baylor.edu>
2022-10-25 16:25:32 -0500

    Storing current user in database and playlist set up in trip dashboard

---
+++ road-trip-api/src/main/java/road/trip/api/persistence/CurUser.java
@@ -0,0 +1,16 @@
+package road.trip.api.persistence;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = User.TABLE_NAME)
+@AllArgsConstructor
+public class CurUser extends User {
+    public static final String TABLE_NAME = "CurUser";
+}
