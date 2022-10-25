a08da3399e5c2d7b60cbd1c0b663735bdfa6e92d
Mia Gortney <mia_gortney1@baylor.edu>
2022-10-25 17:58:00 -0500

    Bug fixes

--- road-trip-api/src/main/java/road/trip/api/persistence/CurUser.java
+++ road-trip-api/src/main/java/road/trip/api/persistence/CurUser.java
@@ -7,11 +7,9 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-@Data
 @Entity
 @Table(name = User.TABLE_NAME)
 @AllArgsConstructor
-@NoArgsConstructor
 public class CurUser extends User {
     public static final String TABLE_NAME = "CurUser";
 }
