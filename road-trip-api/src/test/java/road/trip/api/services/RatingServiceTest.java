78871de007cac975fdb82fcf648abc158bd551d7
Joe Cranney <jcranney11@gmail.com>
2022-12-05 14:34:31 -0600

    Rating service test

--- road-trip-api/src/test/java/road/trip/api/services/RatingServiceTest.java
+++ road-trip-api/src/test/java/road/trip/api/services/RatingServiceTest.java
@@ -5,7 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import road.trip.api.persistence.Rating;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
 public class RatingServiceTest {
@@ -13,10 +17,28 @@ public class RatingServiceTest {
     @Autowired
     RatingService ratingService;
 
-    /*
     @Test
-    public void testGetRatings() throws Exception {
-        assertNotNull(ratingService.getRatings());
+    void testGetRatings_NoException() {
+        assertDoesNotThrow(() -> {
+            ratingService.getRatings();
+        });
     }
-*/
+
+    @Test
+    void testGetRatingsByTripId_ValidId() {
+        List<Rating> ratingList = ratingService.getRatingsByTripId(163L);
+        assertThat(ratingList).hasSize(3);
+    }
+
+    @Test
+    void testGetRatingsByTripId_InvalidId() {
+        List<Rating> ratingList = ratingService.getRatingsByTripId(-1L);
+        assertThat(ratingList).isEmpty();
+    }
+
+    @Test
+    void testSaveRating_Valid() {
+        Rating rating = new Rating(null, null, 4, "Test rating", null, "trip", new ArrayList<>());
+        
+    }
 }
