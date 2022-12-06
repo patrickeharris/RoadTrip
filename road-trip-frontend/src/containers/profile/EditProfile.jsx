fe874c19c46f9c719077d03a22a0c557591782f4
Jack Hale <jackhale@gmail.com>
2022-12-05 21:21:41 -0600

    edit profile troubleshooting

--- road-trip-frontend/src/containers/profile/EditProfile.jsx
+++ road-trip-frontend/src/containers/profile/EditProfile.jsx
@@ -108,14 +108,9 @@ const EditProfile = () => {
                         <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} value={email}/>
                         <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password}/>
                         <input type="password" placeholder="Confirm Password" onChange={(e) => setConfirm(e.target.value)} value={confirm}/>
-                        <div class="row">
-                            <div class="column">
-                                <img className="h-20 w-20 rounded-full" src="/static/girlProfile.png"/>
-                            </div>
-                            <div className="column">
-                                <img className="h-20 w-20 rounded-full" src="/static/guyProfile.png"/>
-                            </div>
-                        </div>
+
+                        <img className="h-20 w-20 rounded-full" src="/static/girlProfile.png"/>
+                        <img className="h-20 w-20 rounded-full" src="/static/guyProfile.png"/>
 
                         <button type="button" onClick={handleSubmit}>Update Profile</button>
                     </div>
